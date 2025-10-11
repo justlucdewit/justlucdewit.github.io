f87a1bd0407061e9e723de07bb5799305c3895d6
Luc de Wit <lucdewit@macbookpro.home>
2025-10-11 17:01:48 +0200

    Header submenu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
 import { useLocation } from 'preact-iso';
+import { useState } from 'preact/hooks';
 
 export function Header() {
 	const { url } = useLocation();
+
+	const [openTab, setOpenTab] = useState(null);
 
 	return (
 		<header>
@@ -15,9 +18,27 @@ export function Header() {
 						Home
 					</a>
 
-					{/* <a href="#/activity" class={url == '/' && 'active'}>
+					<a class={url == '/' && 'active'} onClick={() => openTab == 'activity' ? setOpenTab(null) : setOpenTab('activity')}>
 						Activity
-					</a> */}
+
+						{
+							openTab == 'activity' && (
+								<div className="sub-menu">
+									<a href="#/activity/speedcubing" class={url == '/' && 'active'}>
+										Speedcubing
+									</a>
+
+									<a href="#/activity/dreaming" class={url == '/' && 'active'}>
+										Dreaming
+									</a>
+
+									<a href="#/activity/programming" class={url == '/' && 'active'}>
+										Programming
+									</a>
+								</div>
+							)
+						}
+					</a>
 
 					<a href="#/resume" class={url == '/' && 'active'}>
 						Resume
